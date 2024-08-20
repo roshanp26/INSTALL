d9c809102dc322411dbb504cdf82b2f82ddc594d
Roshan Patil <84975574+roshan2610@users.noreply.github.com>
2024-08-20 10:45:18 +0530

    Update Install-docker.sh

--- DOCKER/Install-docker.sh
+++ DOCKER/Install-docker.sh
@@ -13,7 +13,7 @@ echo \
 sudo apt-get update -y
 
 #To install the latest version, run:
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 #To give permission for user
 sudo chown $USER  /var/run/docker.sock
